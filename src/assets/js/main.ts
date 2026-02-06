9f298b1467bcf68fb505ea3b7541da73cf405d52
chiilog <purgexpurge@gmail.com>
2026-02-06 17:30:13 +0900

    njkファイル作成

---
+++ src/assets/js/main.ts
@@ -0,0 +1,13 @@
+// メインJavaScriptエントリーポイント
+
+// CSS のインポート
+import '../css/style.css';
+
+console.log('Hello from main.ts');
+
+// 機能ごとにファイルを分割してインポートする例:
+// import { initHamburgerMenu } from './hamburger';
+// import { initAccordion } from './accordion';
+//
+// initHamburgerMenu();
+// initAccordion();
